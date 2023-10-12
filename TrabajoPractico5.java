f68c5be11d39c3548ff8aeb9f924f54b3b869c8e
PaulaGeier <107880702+PaulaGeier@users.noreply.github.com>
2023-10-12 08:54:18 -0300

    TP5

--- TrabajoPractico5.java
+++ TrabajoPractico5.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class TrabajoPractico3 {
+public class TrabajoPractico5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
