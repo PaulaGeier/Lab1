77c8137197e3e02bc6064f9f841c16cc76b10395
PaulaGeier <107880702+PaulaGeier@users.noreply.github.com>
2023-10-12 08:53:13 -0300

    TP6

--- TrabajoPractico6.java
+++ TrabajoPractico6.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
-public class TrabajoPractico4 {
+public class TrabajoPractico6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
